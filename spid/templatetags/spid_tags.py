210cd2c1188e11d4a38d6576213d2abc2ed810f1
Elin Wedlund <ewedlund@fub.it>
2020-10-19 11:00:48 +0200

    merged changes

--- spid/templatetags/spid_tags.py
+++ spid/templatetags/spid_tags.py
@@ -6,23 +6,41 @@ from ..apps import SpidConfig
 
 register = template.Library()
 
-SPID_BUTTON_SIZES = {'small', 'medium', 'large', 'xlarge'}
+SPID_BUTTON_SIZES = {"small", "medium", "large", "xlarge"}
 
 
 @register.inclusion_tag("spid_button.html", takes_context=True)
-def spid_button(context, size='medium'):
+def spid_button(context, size="medium", index="0"):
     if size not in SPID_BUTTON_SIZES:
-        raise ValueError("argument 'size': value %r not in %r." % (size, SPID_BUTTON_SIZES))
+        raise ValueError(
+            "argument 'size': value %r not in %r." % (size, SPID_BUTTON_SIZES)
+        )
 
     spid_idp_list = [
-        {'id': k, 'name': v['name']}
-        for k, v in SpidConfig.identity_providers.items()
+        {"id": k, "name": v["name"]} for k, v in SpidConfig.identity_providers.items()
     ]
     random.shuffle(spid_idp_list)
     return {
-        'method': context['request'].method.lower(),
-        'post_data': context['request'].POST,
-        'spid_button_size': size,
-        'spid_button_size_short': size[0] if size != 'xlarge' else size[:2],
-        'spid_idp_list': spid_idp_list
+        "method": context["request"].method.lower(),
+        "post_data": context["request"].POST,
+        "spid_button_size": size,
+        "index": index,
+        "spid_button_size_short": size[0] if size != "xlarge" else size[:2],
+        "spid_idp_list": spid_idp_list,
+    }
+
+
+@register.inclusion_tag("eid_button.html", takes_context=True)
+def eid_button(context, idp_id="eid_test", size="medium", index="1"):
+    if size not in SPID_BUTTON_SIZES:
+        raise ValueError(
+            "argument 'size': value %r not in %r." % (size, SPID_BUTTON_SIZES)
+        )
+    return {
+        "method": context["request"].method.lower(),
+        "post_data": context["request"].POST,
+        "spid_button_size": size,
+        "index": index,
+        "idp_id": idp_id,
+        "spid_button_size_short": size[0] if size != "xlarge" else size[:2],
     }
